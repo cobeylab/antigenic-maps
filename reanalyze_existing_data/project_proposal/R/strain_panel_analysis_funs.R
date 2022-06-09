636731a1a0e7bb6e87bc25be734e956f6109e075
kgostic <kategostic@gmail.com>
2022-06-08 17:01:04 -0700

    Added code and notebook to design strain panel

--- reanalyze_existing_data/project_proposal/R/strain_panel_analysis_funs.R
+++ reanalyze_existing_data/project_proposal/R/strain_panel_analysis_funs.R
@@ -13,7 +13,10 @@
 # * exposure_no - primary or secondary
 # * exposure_type - vaccine or infection
 
-source('../Ha_Nam/R/analysis_functions.R')
+to_log = function(titer){
+  log2(titer/10)
+}
+
 
 select_standard_cols = function(df){
   df %>%
@@ -36,7 +39,7 @@ select_standard_cols = function(df){
            logtiter = as.numeric(logtiter), 
            is_cross_reactive = as.logical(is_cross_reactive),
            dt = as.numeric(dt), 
-           past_future = as.character(past_future), 
+           past_future = factor(past_future, levels = c('past', 'future', 'concurrent')), 
            host_type = as.character(host_type), 
            exposure_no = as.character(exposure_no), 
            exposure_type = as.character(exposure_type), 
@@ -48,9 +51,9 @@ get_past_future = function(homologous_strain_year, test_strain_year){
          ifelse(homologous_strain_year<test_strain_year, 'future', 'concurrent'))
 }
 
-import_HaNam_PCR_infections <- function(cross_reactivity_threshold = 40){
+import_HaNam_PCR_infections <- function(cross_reactive_threshold = 40){
   source('../../Ha_Nam/R/analysis_functions.R')
-  raw_df = read_csv('../../Ha_Nam/raw_data/Fonville_2014_TableS3.csv') %>% ## HaNam cohort titers
+  raw_df = read_csv('../../Ha_Nam/raw_data/Fonville_2014_TableS3.csv', show_col_types = F) %>% ## HaNam cohort titers
     mutate_at(-c(1,2), .funs = ~HaNam_titers_to_numeric(.)) %>% ## Convert titer values to numeric. Report undetectable = 5, and endpoint = 1280
     pivot_longer(-c(1,2), names_to = 'test_strain', values_to = 'titer') %>%
     dplyr::filter(!test_strain == 'A/PHILIPPINES/472/2002_EGG') %>% ## Filter out the Egg adapted version of A/PHILIPPINES/472/2002
@@ -58,8 +61,8 @@ import_HaNam_PCR_infections <- function(cross_reactivity_threshold = 40){
     mutate(logtiter = to_log(titer)) %>%
     tidyr::extract(col = test_strain, into = 'test_strain_year', regex = '/(\\d+)$', remove = F) %>%
     mutate(test_strain_year = clean_years(test_strain_year)) %>%
-    merge(read_csv('../../Ha_Nam/processed_data/YOB_inferred.csv'), by = 'Subject number') %>% ## Merge with Kangchon's annotation of PCR-confirmed infections %>%
-    merge(read_csv('../../Ha_Nam/processed_data/HaNam_strains.csv'), by = 'test_strain') %>%
+    merge(read_csv('../../Ha_Nam/processed_data/YOB_inferred.csv', show_col_types = F), by = 'Subject number') %>% ## Merge with Kangchon's annotation of PCR-confirmed infections %>%
+    merge(read_csv('../../Ha_Nam/processed_data/HaNam_strains.csv', show_col_types = F), by = 'test_strain') %>%
     mutate(test_strain = full_name) %>%
     dplyr::filter(`PCR+` == 'PCR+') %>%
     dplyr::filter(`Sample year` == `Infection Time`) %>%
@@ -77,13 +80,13 @@ import_HaNam_PCR_infections <- function(cross_reactivity_threshold = 40){
              host_type = 'human',
              exposure_no = 'secondary',
              exposure_type = 'infection',
-             is_cross_reactive = ifelse(logtiter >= to_log(cross_reactivity_threshold), TRUE, FALSE)) 
+             is_cross_reactive = ifelse(logtiter >= to_log(cross_reactive_threshold), TRUE, FALSE)) 
     select_standard_cols(raw_df)
 }
 
-import_HaNam_seroconversions <- function(cross_reactivity_threshold = 40){
+import_HaNam_seroconversions <- function(cross_reactive_threshold = 40){
   source('../../Ha_Nam/R/analysis_functions.R')
-  raw_df = read_csv('../../Ha_Nam/raw_data/Fonville_2014_TableS3.csv') %>% ## HaNam cohort titers
+  raw_df = read_csv('../../Ha_Nam/raw_data/Fonville_2014_TableS3.csv', show_col_types = F) %>% ## HaNam cohort titers
     mutate_at(-c(1,2), .funs = ~HaNam_titers_to_numeric(.)) %>% ## Convert titer values to numeric. Report undetectable = 5, and endpoint = 1280
     pivot_longer(-c(1,2), names_to = 'test_strain', values_to = 'titer') %>%
     dplyr::filter(!test_strain == 'A/PHILIPPINES/472/2002_EGG') %>% ## Filter out the Egg adapted version of A/PHILIPPINES/472/2002
@@ -91,26 +94,26 @@ import_HaNam_seroconversions <- function(cross_reactivity_threshold = 40){
     mutate(logtiter = to_log(titer)) %>%
     tidyr::extract(col = test_strain, into = 'test_strain_year', regex = '/(\\d+)$', remove = F) %>%
     mutate(test_strain_year = clean_years(test_strain_year)) %>%
-    merge(read_csv('../../Ha_Nam/processed_data/YOB_inferred.csv'), by = 'Subject number') %>% ## Merge with Kangchon's annotation of PCR-confirmed infections %>%
-    merge(read_csv('../../Ha_Nam/processed_data/HaNam_strains.csv'), by = 'test_strain') %>%
+    merge(read_csv('../../Ha_Nam/processed_data/YOB_inferred.csv', show_col_types = F), by = 'Subject number') %>% ## Merge with Kangchon's annotation of PCR-confirmed infections %>%
+    merge(read_csv('../../Ha_Nam/processed_data/HaNam_strains.csv', show_col_types = F), by = 'test_strain') %>%
     dplyr::filter(Seroconversion) %>%
     dplyr::filter(`Sample year` == `Infection Time`) %>%
     dplyr::filter(!is.na(titer)) %>%
     rename(id = `Subject number`,
            sample_year = `Sample year`,
            homologous_strain_year = `Infection Time`) %>%
-    mutate(dataset = 'HaNam_PCR_infections',
+    mutate(dataset = 'HaNam_seroconversions',
            dt = abs(homologous_strain_year-test_strain_year),
            past_future = get_past_future(homologous_strain_year, test_strain_year),
            host_type = 'human',
            exposure_no = 'secondary',
            exposure_type = 'infection',
-           is_cross_reactive = ifelse(logtiter >= to_log(cross_reactivity_threshold), TRUE, FALSE),
+           is_cross_reactive = ifelse(logtiter >= to_log(cross_reactive_threshold), TRUE, FALSE),
            homologous_strain = NA) 
   select_standard_cols(raw_df)
 }
 
-import_HaNam_vaccine_study <- function(cross_reactivity_threshold = 40){
+import_HaNam_vaccine_study <- function(cross_reactive_threshold = 40){
   source('../../Ha_Nam/R/analysis_functions.R')
  raw_df =  bind_rows(
     ## 1995 vaccine study
@@ -120,7 +123,7 @@ import_HaNam_vaccine_study <- function(cross_reactivity_threshold = 40){
       mutate(homologous_strain_year = 1997)
   ) %>%
     mutate(dataset = 'HaNam_vaccine',
-           is_cross_reactive = ifelse(logtiter >= to_log(cross_reactivity_threshold), TRUE, FALSE),
+           is_cross_reactive = ifelse(logtiter >= to_log(cross_reactive_threshold), TRUE, FALSE),
            dt = abs(homologous_strain_year - test_strain_year),
            past_future = get_past_future(homologous_strain_year, test_strain_year),
            exposure_no = 'secondary',
@@ -173,7 +176,59 @@ import_Fonville_2016_ferret_data <- function(cross_reactive_threshold = 40){
     select_standard_cols()
 }
 
-import_Bedford_2014_ferret_data <- function(cross_reactive_threshold = 40){
+
+
+import_Bedford_2014_data <- function(cross_reactive_threshold=40){
+  parse_undetectable = function(und_titer){
+    min_dilution = gsub('<(\\d+)', '\\1', und_titer) %>% as.numeric() ## Get the minimum measured dilution
+    ifelse(min_dilution <= 10, 
+           to_log((min_dilution)/2), ## Report half the measured min dilution
+           ifelse(min_dilution <= 40, ## if between 10 and 40, report half the min dilution minus 0.5
+                  to_log(min_dilution/2)-.5,
+           NA)) ## Else don't report
+  }
+  titers_to_numeric_Bedford = function(titers){
+    is_numeric = !grepl("<", titers)
+    titers[is_numeric] = to_log(as.numeric(titers[is_numeric]))
+    titers[!is_numeric] = parse_undetectable(titers[!is_numeric])
+    as.numeric(titers)
+  }
+  read_delim(file = '../../map_refitting/raw-data/Bedford_2014_data.txt', show_col_types = F) %>%
+    rename(test_strain = virusStrain,
+           test_strain_year = virusYear,
+           homologous_strain = serumStrain,
+           homologous_strain_year = serumYear) %>%
+    mutate(id = as.numeric(as.factor(sprintf('%s-%s', serumIsolate, source))),
+           logtiter = titers_to_numeric_Bedford(titer),
+           is_cross_reactive = ifelse(logtiter>=to_log(cross_reactive_threshold), TRUE, FALSE),
+           dt = abs(homologous_strain_year-test_strain_year),
+           past_future = get_past_future(homologous_strain_year, test_strain_year),
+           host_type = 'ferret',
+           exposure_no = 'primary',
+           exposure_type = 'infection',
+           dataset = 'Bedford_2014') %>%
+    select_standard_cols() %>%
+    filter(!is.na(logtiter))
+}
+
+plot_cr_bars <- function(df){
+  df %>%
+    filter(!is.na(logtiter)) %>%
+    mutate(dt = homologous_strain_year-test_strain_year) %>%
+    group_by(dt, is_cross_reactive) %>%
+    summarise(count = n()) %>%
+    group_by(dt) %>%
+    mutate(fraction = count/sum(count),
+           lower = fraction - 1.96*sqrt(fraction*(1-fraction)/sum(count)),
+           upper = fraction + 1.96*sqrt(fraction*(1-fraction)/sum(count)),
+           lower = ifelse(is_cross_reactive, lower, NA),
+           upper = ifelse(is_cross_reactive, upper, NA))%>%
+    ggplot() +
+    geom_bar(aes(x = dt, y = fraction, fill = is_cross_reactive), stat = 'identity', alpha = .5)+
+    geom_segment(aes(x = dt, xend = dt, y = lower, yend = upper))+
+    geom_vline(aes(xintercept = 0), lty = 2) +
+    ggtitle(sprintf('%s, %s response', unique(df$host_type), unique(df$exposure_no)), subtitle = sprintf('%s', unique(df$dataset)))+
+    xlim(c(-40, 40))
 }
 
 
